1558e0918db1c666d9a08b08053d54e568fdaf15
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-19 09:00:00 +0100

    feat(user): add Role enum (ADMIN, AGRICULTEUR, ONG)

---
+++ AquaSmart-App/user-service/src/main/java/com/aquasmart/userservice/model/enums/Role.java
@@ -0,0 +1,10 @@
+package com.aquasmart.userservice.model.enums;
+
+/**
+ * Énumération des rôles utilisateurs dans AquaSmart
+ */
+public enum Role {
+    ADMIN,       // Administrateur de la plateforme
+    AGRICULTEUR, // Agriculteur / Exploitant
+    ONG          // Organisation non gouvernementale
+}
