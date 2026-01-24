eb0950a2585af5aff31d71cd2e6fe2cf600e659f
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2026-01-24 14:00:00 +0100

    feat(user): add password reset DTOs

---
+++ AquaSmart-App/user-service/src/main/java/com/aquasmart/userservice/dto/request/ForgotPasswordRequest.java
@@ -0,0 +1,20 @@
+package com.aquasmart.userservice.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO pour la demande de réinitialisation de mot de passe
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForgotPasswordRequest {
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Format d'email invalide")
+    private String email;
+}
