d9dfe6dab7ac5589cc7e78829381837696827c41
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2026-01-30 14:00:00 +0100

    feat(farm): add exception handling

---
+++ AquaSmart-App/farm-service/src/main/java/com/aquasmart/farmservice/exception/BadRequestException.java
@@ -0,0 +1,12 @@
+package com.aquasmart.farmservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
